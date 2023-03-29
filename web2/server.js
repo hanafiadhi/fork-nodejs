b400fa710b2365e003625d7fa5eabe8883cc96a2
Hanafi <afiban231@gmail.com>
2023-03-29 10:20:54 +0700

    final feature

---
+++ web2/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const redis = require("redis");
+const app = express();
+
+const redisCLient = redis.createClient({
+  host: "redis",
+  port: 6379,
+});
+
+app.get("/", function (req, res) {
+  redisCLient.get("keyVisitor", function (err, numVisitor) {
+    let displayvisitor = parseInt(numVisitor) + 1;
+    if (isNaN(displayvisitor)) {
+      displayvisitor = 1;
+    }
+    res.send("web2: Total number of visitors is" + displayvisitor);
+    redisCLient.set("keyVisitor", displayvisitor);
+  });
+});
+
+app.listen(5000, () => {
+  console.log("web1 is listening on port 5000");
+});
